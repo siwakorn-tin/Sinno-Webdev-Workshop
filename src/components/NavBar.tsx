a936cb5d3aeba717d9212ebeb366c6b24f5d34b7
siwakorn-tin <siwakorntin@outlook.com>
2024-03-30 02:24:26 +0700

    Update NavBar.tsx

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,9 @@ function NavBar() {
   return (
     <nav className="flex gap-x-4 pt-2 pb-10">
       {menu.map((item) => (
-        <Link href={item.link}>{item.name}</Link>
+        <Link key={item.name} href={item.link}>
+          {item.name}
+        </Link>
       ))}
     </nav>
     // <nav className="top-0 sticky flex gap-x-4 bg-black mt-2 p-2 w-[100vw]">
